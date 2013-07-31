cc4aca09f6964fce15b1bedd5dc0779627e8d2de
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2013-07-31 08:31:55 +0200

    Improve api

--- README.md
+++ README.md
@@ -2,9 +2,6 @@ pagination
 ==========
 
 ```javascript
-var numberOfHits = 25;
-var perPage = 10;
-
-var pages = pagination(numberOfHits, perPage);
+var pages = pagination({ total: 25, rows: 10 });
 // pages = [1, 2, 3]
 ```
--- test/paginationTest.js
+++ test/paginationTest.js
@@ -5,32 +5,32 @@ var expect = chai.expect;
 describe('pagination', function() {
 
     it('returns empty array if total hits is 0', function() {
-        var pages = pagination(0, 10);
+        var pages = pagination({ total: 0, rows: 10 });
         expect(pages).to.be.empty;
     });
 
     it('returns empty array if hits per page is less than total hits', function() {
-        var pages = pagination(1, 10);
+        var pages = pagination({ total: 1, rows: 10 });
         expect(pages).to.be.empty;
     });
 
     it('returns array with two pages', function() {
-        var pages = pagination(15, 10);
+        var pages = pagination({ total: 15, rows: 10 });
         expect(pages).to.eql([1,2]);
     });
 
     it('returns array with two pages is total hits is the double of hits per page', function() {
-        var pages = pagination(20, 10);
+        var pages = pagination({ total: 20, rows: 10 });
         expect(pages).to.eql([1,2]);
     });
 
     it('returns array with three pages', function() {
-        var pages = pagination(21, 10);
+        var pages = pagination({ total: 21, rows: 10 });
         expect(pages).to.eql([1,2,3]);
     });
 
     it('returns array with 100 pages', function() {
-        var pages = pagination(1000, 10);
+        var pages = pagination({ total: 1000, rows: 10 });
         expect(pages.length).to.eql(100);
     });
 
